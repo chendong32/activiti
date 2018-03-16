8bde36694e0bf1c66fc634df8ec78b24f96fcc9a
chendong32 <434611969@qq.com>
2018-03-16 21:13:51 +0800

    数据库信息

---
+++ src/main/resources/sql/test.sql
@@ -0,0 +1,45 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.5.27 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `test_data` */
+
+DROP TABLE IF EXISTS `test_data`;
+
+CREATE TABLE `test_data` (
+  `id` varchar(64) NOT NULL DEFAULT '' COMMENT '主键',
+  `create_by` varchar(64) DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT NULL COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注信息',
+  `del_flag` varchar(64) DEFAULT NULL COMMENT '逻辑删除标记（0：显示 1：隐藏）',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `test_data` */
+
+LOCK TABLES `test_data` WRITE;
+
+insert  into `test_data`(`id`,`create_by`,`create_date`,`update_by`,`update_date`,`remarks`,`del_flag`) values ('1',NULL,'2018-03-16 21:01:35',NULL,'2018-03-16 21:01:35',NULL,NULL);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
