f5a48ddc246e1234434bf865782d5a3dca81d13c
chendong32 <434611969@qq.com>
2018-04-13 17:44:49 +0800

    后台提供服务接口

--- src/main/java/com/entity/TestUser.java
+++ src/main/java/com/entity/TestUser.java
@@ -2,4 +2,44 @@ package com.entity;
 
 public class TestUser {
 	protected String id;
+	protected String name;
+	protected String age;
+	
+	public TestUser() {
+		super();
+	}
+
+	public TestUser(String id, String name, String age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	
+
 }
--- src/main/java/com/web/TestController.java
+++ src/main/java/com/web/TestController.java
@@ -1,15 +1,35 @@
 package com.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSON;
+import com.entity.TestUser;
 
 @Controller
 public class TestController {
 	
-	@RequestMapping("/login")
+	@ResponseBody
+	@RequestMapping(value = "/login", produces = "application/json; charset=utf-8")
 	public String doLogin(){
-		
-		return "sys/login";
+		Map<String, List<TestUser>> map = new HashMap<>();
+		TestUser t1 = new TestUser("1","王菲","11");
+		TestUser t2 = new TestUser("2","测试","12");
+		TestUser t3 = new TestUser("3","可以","13");
+		TestUser t4 = new TestUser("4","跨域","14");
+		List<TestUser> list = new ArrayList<>();
+		list.add(t1);
+		list.add(t2);
+		list.add(t3);
+		list.add(t4);
+		map.put("data", list);
+		return JSON.toJSONString(map);
 	}
 
 }
